7135ab3897c8c13dfb0acf115d16ebf0c38b7f27
Norbert <norbetltagne@gmail.com>
2025-04-10 09:17:22 +0100

    mini api avec nest, express et ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors"; // Uncomment if you need to use CORS
+//import cors from "cors"; // Uncomment if you need to use CORS
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors);
+//app.use(cors);
 app.use(express.json());
 
 // Route de Test
