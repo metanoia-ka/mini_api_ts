39910449527146d06838cd7e262de330377135ce
Norbert <norbetltagne@gmail.com>
2025-04-10 09:14:59 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors"; // Uncomment if you need to use CORS
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors);
+app.use(express.json());
+
+// Route de Test
+app.get("/", (req, res) => {
+  res.send("🚀 API TypeScript avec Express est en ligne !");
+});
+
+// Lancer le serveur
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur lancé sur le port ${PORT}`);
+});
